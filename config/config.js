b59698f7c391398d0a4b3eb69193b0e45f8d3b84
= <=>
2018-12-27 15:05:16 -0300

    Set node_env to keys and add node engine version

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
-const { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_PASSWORD } = require('./keys');
-
-if (SPOTIFY_CLIENT_ID !== null && SPOTIFY_CLIENT_PASSWORD !== null) {
+if (process.env.NODE !== 'production') {
+  const { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_PASSWORD } = require('./keys');
+  
   module.exports = {
     SPOTIFY_CLIENT_ID,
     SPOTIFY_CLIENT_PASSWORD
